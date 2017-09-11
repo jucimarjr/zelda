99f6af178122f258d28f5980ff0f7e3cc54d0ce8
Kid Mendes <kid.mon@hotmail.com>
2017-09-10 20:21:13 -0400

    Alterações e adição de paginas

--- app/views.py
+++ app/views.py
@@ -7,3 +7,11 @@ from app import app
 def index():
     return render_template("index.html",
                            title='Home',)
+
+@app.route('/cadastrar_funcionario')
+def cadastroFuncionario():
+    return render_template("CadastroFuncionario.html",title='Cadastrar Funcionario')
+
+@app.route('/editar_funcionario')
+def editarFuncionario():
+    return render_template("EditarFuncionario.html",title='Editar Funcionario')

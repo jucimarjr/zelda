9d9077412ccb59b61d70733cbd47b672c8c25850
Rodrigo Moraes <rdcmdev@gmail.com>
2017-09-16 10:51:53 -0400

    refactor: representação das classe modelos como string

--- app/funcionario.py
+++ app/funcionario.py
@@ -1,19 +1,40 @@
 class Funcionario:
+    def __init__(self,
+                 id=0,
+                 nome="none",
+                 login="none",
+                 situacao="none",
+                 setor_id="none",
+                 setor_nome="none",
+                 setor_situacao=0,
+                 senha="none"):
+        self.id = id
+        self.nome = nome
+        self.login = login
+        self.situacao = situacao
+        self.setor_id = setor_id
+        self.setor_nome = setor_nome
+        self.setor_situacao = setor_situacao
+        self.senha = senha
 
-  def __init__(self,
-               id=0,
-               nome="none",
-               login="none",
-               situacao="none",
-               setor_id="none",
-               setor_nome="none",
-               setor_situacao=0,
-               senha="none"):
-    self.id = id
-    self.nome = nome
-    self.login = login
-    self.situacao = situacao
-    self.setor_id = setor_id
-    self.setor_nome = setor_nome
-    self.setor_situacao = setor_situacao
-    self.senha = senha    
+    def __str__(self):
+        string = "{"
+        string += "id:" + str(self.id) + ","
+        string += "nome:" + self.nome + ","
+        string += "login:" + self.login + ","
+        string += "situacao:" + str(self.situacao) + ","
+        string += "setor_id:" + str(self.setor_id) + ","
+        string += "setor_nome:" + self.setor_nome + ","
+        string += "setor_situacao:" + str(self.setor_situacao) + ","
+        string += "senha:" + self.senha
+        string += "}"
+
+        return string
+
+    def __repr__(self):
+        return self.__str__()
+
+
+if __name__ == '__main__':
+    funcionario = Funcionario()
+    print(funcionario)

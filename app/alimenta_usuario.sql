71c0594966a930c0cb84a6fb4e3b062bc294e673
Jailson Pereira <jailsoncolares@gmail.com>
2017-09-17 16:03:25 -0400

    feat: Cadastra usuarios no BD

---
+++ app/alimenta_usuario.sql
@@ -0,0 +1,163 @@
+#insert usuarios
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ( 'andre', 'andre123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('andrea', 'andrea123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('antonio', 'antonio123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('cassio', 'cassio123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('charles', 'charles123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('clairon', 'clairon123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('claudio', 'claudio123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('cleinaldo', 'cleinaldo123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('cleto', 'cleto123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('cristine', 'cristine123',1,1);
+ 
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('danielle', 'danielle123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('eduardo', 'eduardo123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('elisabete', 'elisabete123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('emanuel', 'emanuel123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('fabian', 'fabian123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('felicien', 'felicien123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('gilberto', 'gilberto123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('helio', 'helio123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('ingrid', 'ingrid123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jamile', 'jamile123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('joao', 'joao123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jorge.b', 'jorge.b123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jorge.p', 'jorge.p123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jose.a', 'jose.a123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jose.l', 'jose.l123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jozias', 'jozias123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jucimar', 'jucimar123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('luciano', 'luciano123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('marcia', 'marcia123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ( 'mario', 'mario123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('nadja', 'nadja123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('odwald', 'odwald123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('orlem', 'orlem123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('raimundo', 'raimundo123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('ricardo', 'ricardo123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('roberto', 'roberto123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('rubelmar', 'rubelmar123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('samara', 'samara123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('sergio', 'sergio123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('ubirajara', 'ubirajara123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('walter', 'walter123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('ana.c', 'ana.c123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('anderson', 'anderson123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('araci', 'araci123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('conceicao', 'conceicao123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('eliane', 'eliane123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('jeane', 'jeane123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('joelma', 'joelma123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('lizete', 'lizete123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('lucilene', 'lucilene123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('robson', 'robson123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('ruan', 'ruan123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('sandy', 'sandy123',1,1);
+
+INSERT INTO usuario(usuario_login, usuario_senha, usuario_logado, usuario_admin) VALUES
+ ('willem', 'willem123',1,1);

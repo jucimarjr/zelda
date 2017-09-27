c0d235b7faa2bbf2cb3f63c220fd34646821d992
PSFreitas <pssfjr@gmail.com>
2017-09-27 17:00:33 -0400

    refactor: código conforme pep8 e snake_case

--- app/forms.py
+++ app/forms.py
@@ -6,25 +6,30 @@ from passlib.hash import sha256_crypt
 from functools import wraps
 from wtforms.validators import DataRequired
 
-#Cadastra Login
+
+# Cadastra Login
 class LoginForm(Form):
     login = StringField('Nome de Usuário', validators=[DataRequired('Nome de Usuário é obrigatório')])
     senha = PasswordField('Senha', validators=[DataRequired('Senha é obrigatório')])
 
-#Cadastra Funcionario
+
+# Cadastra Funcionario
 class CadastraFuncionarioForm(Form):
     funcionario_nome = StringField('Nome Funcionário', validators=[DataRequired('Nome de Funcionário é obrigatório')])
-    funcionario_setor_id = SelectField ('Setor', coerce=int)
-    
-#Cadastra Usuario
+    funcionario_setor_id = SelectField('Setor', coerce=int)
+
+
+# Cadastra Usuario
 class CadastraUsuarioForm(Form):
     usuario_login = StringField('Login Usuario', validators=[DataRequired('Login do Usuario é obrigatório')])
     usuario_senha = PasswordField('Senha', validators=[DataRequired('Senha do Usuario é obrigatório')])
-    usuario_admin = RadioField('Tipo do Usuário', validators=[DataRequired('Tipo do Usuário é obrigatório')], choices=[ (1, 'Administrador'),(2, 'Usuário Comum')], coerce=int)
-    
-#Cadastra Setor
+    usuario_admin = RadioField('Tipo do Usuário', validators=[DataRequired('Tipo do Usuário é obrigatório')], choices=[(1, 'Administrador'), (2, 'Usuário Comum')], coerce=int)
+
+
+# Cadastra Setor
 class CadastraSetorForm(Form):
     setor_nome = StringField('Nome Setor', validators=[DataRequired('Nome do Setor é obrigatório')])
+
 
 # Atualiza Setor
 class AtualizaSetorForm(Form):
@@ -34,24 +39,27 @@ class AtualizaSetorForm(Form):
     setor_id = HiddenField('ID Setor', validators=[DataRequired('O ID do Setor não pode ser indefinido')])
 
 
-#Atualiza Funcionario
+# Atualiza Funcionario
 class AtualizaFuncionarioForm(Form):
     funcionario_nome = StringField('Nome Funcionário', validators=[DataRequired('Nome de Funcionário é obrigatório')])
     lotacao_id = HiddenField('ID Lotação', validators=[])
-    setor_id = SelectField ('Setor', coerce=int)
+    setor_id = SelectField('Setor', coerce=int)
     funcionario_id = HiddenField('ID Funcionário', validators=[DataRequired('O ID do Funcionário não pode ser indefinido')])
+
 
 # Atualiza Usuário
 class AtualizaUsuarioForm(Form):
     usuario_login = StringField('Login do Usuário', validators=[DataRequired('Login do Usuário é obrigatório')])
-    usuario_admin = RadioField('Tipo do Usuário', validators=[DataRequired('Tipo do Usuário é obrigatório')], choices=[ (1, 'Administrador'),(2, 'Usuário Comum')], coerce=int)
+    usuario_admin = RadioField('Tipo do Usuário', validators=[DataRequired('Tipo do Usuário é obrigatório')], choices=[(1, 'Administrador'), (2, 'Usuário Comum')], coerce=int)
     usuario_id = HiddenField('ID Usuário', validators=[DataRequired('O ID do Usuário não pode ser indefinido')])
     usuario_senha = PasswordField('Senha do Usuário', validators=[DataRequired('A senha do Usuário é obrigatória')])
+
 
 # Remover Funcionário
 class RemoveFuncionarioForm(Form):
     # Implementa um campo em forma de lista, cujos elementos serão inputs do tipo HiddenField
     funcionarios_ids = FieldList(HiddenField('IDs dos Funcionários', validators=[DataRequired('Os IDs da lista não podem ser indefinidos')]), validators=[DataRequired('A lista de IDs não pode ser indefinida')])
+
 
 # Remover Setor
 class RemoveSetorForm(Form):
--- app/lotacao.py
+++ app/lotacao.py
@@ -1,12 +1,8 @@
 class Lotacao:
-    def __init__(self,
-                 id=0,
-                 funcionario_id=0,
-                 setor_id=0):
-                   self.id = id
-                   self.funcionario_id = funcionario_id
-                   self.setor_id = setor_id
-        
+    def __init__(self, id=0, funcionario_id=0, setor_id=0):
+        self.id = id
+        self.funcionario_id = funcionario_id
+        self.setor_id = setor_id
 
     def __str__(self):
         string = "{"
@@ -14,13 +10,11 @@ class Lotacao:
         string += "funcionario_id:" + self.funcionario_id + ","
         string += "setor_id:" + self.setor_id + ","
         string += "}"
-
         return string
 
     def __repr__(self):
         return self.__str__()
 
-
 if __name__ == '__main__':
-	lotacao = Lotacao()
-	print(lotacao)
+    lotacao = Lotacao()
+    print(lotacao)

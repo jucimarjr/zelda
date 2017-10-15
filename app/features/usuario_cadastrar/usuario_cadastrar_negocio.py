7615a3af2486e78232bfba60e1d7fff0aba2448d
BonifacioOliveira <bonifaciooliveirafilho@hotmail.com>
2017-10-15 15:16:19 -0400

    feat: upload foto cadastrar usuario

--- app/features/usuario_cadastrar/usuario_cadastrar_negocio.py
+++ app/features/usuario_cadastrar/usuario_cadastrar_negocio.py
@@ -4,6 +4,15 @@ from ...utils.flash_errors import flash_errors
 from ...tables.usuario.usuario_modelo import Usuario
 from ...utils.criptografador import Criptografador
 from ...cursor import db
+import os
+
+from werkzeug import secure_filename
+from app import app, ALLOWED_EXTENSIONS
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class UsuarioCadastrarNegocio:
     
@@ -13,7 +22,23 @@ class UsuarioCadastrarNegocio:
             usuario = Usuario(login=form.usuario_login.data, senha=Criptografador.gerar_hash(form.usuario_senha.data, ''), admin=form.usuario_admin.data - 1)
 
             db.cadastra_usuario(usuario)
+            
+            if form.file.data is not None:
+                filename = secure_filename(form.file.data.filename)
+
+                if allowed_file(filename):
+                    path = os.path.abspath(os.path.join(app.config['USUARIOS_UPLOAD_PATH'], str(user_id) + '.' + filename.rsplit('.',1)[1]))
+                    form.file.data.save(path)
+                    return redirect(url_for('usuario_listar'))
+                else:
+                    flash("Os formatos da foto são restritos a png, jpg e jpeg")
+
+            else:
+                return redirect(url_for('usuario_listar'))
+            
             return redirect(url_for('usuario_listar'))
+        
         else:
             flash_errors(form)
-            return render_template('usuario_criar.html', form=form)
+        
+        return render_template('usuario_criar.html', form=form)

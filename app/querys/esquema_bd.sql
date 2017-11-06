82b98156a451bd0edede3e6305594cabac30a39d
MikeFP <mikefp28@gmail.com>
2017-11-05 22:04:44 -0400

    refactor: remove o campo usuário logado do projeto

--- app/querys/esquema_bd.sql
+++ app/querys/esquema_bd.sql
@@ -77,10 +77,9 @@ CREATE TABLE usuario (
   usuario_id int(11) AUTO_INCREMENT NOT NULL,
   usuario_login varchar(100) NOT NULL,
   usuario_senha varchar(50),
-  usuario_logado int(11) NOT NULL DEFAULT '0',
   usuario_email varchar(100) NOT NULL,
   usuario_status int(11) NOT NULL DEFAULT '0',
-  usuario_caminho_foto varchar(100) NOT NULL DEFAULT 'user_profile.jpg',
+  usuario_caminho_foto varchar(100),
 
   perfil_id int(11) NOT NULL,
 

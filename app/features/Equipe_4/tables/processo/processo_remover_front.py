6759022cf9b673fe65ee055ff5beda2dcdc98ff2
Calebe Rebello <caleberoberto2@gmail.com>
2017-11-14 03:10:54 -0400

    Delete processo_remover_front.py

--- app/features/Equipe_4/tables/processo/processo_remover_front.py
+++
@@ -1,9 +0,0 @@
-from .processo_remover_negocio import ProcessoRemoverNegocio
-from app import app
-from ....utils.front_helper import *
-
-@app.route('equipe4/processo/remover/<processo_id>', methods=['GET', 'POST'])
-@login_required
-@verifica_permissao
-def processo_remover(processo_id):
-    return ProcessoRemoverNegocio.exibir(processo_id)

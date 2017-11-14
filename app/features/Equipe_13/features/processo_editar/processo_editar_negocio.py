b68d33ccc9389da853c5048d9c7ca2a4abd07233
caniiggia <caniiggia2010@hotmail.com>
2017-11-14 02:54:46 -0400

    fix: editar_processo

--- app/features/Equipe_13/features/processo_editar/processo_editar_negocio.py
+++ app/features/Equipe_13/features/processo_editar/processo_editar_negocio.py
@@ -1,31 +1,29 @@
+from flask import render_template, flash, redirect, url_for, request
 from .processo_editar_form import EditarProcessoForm
 from ..features.Equipe_13.tables.processo.processo_modelo import Processo
-from ....utils.flash_errors import flash_errors
-from ....utils.zelda_modelo import ZeldaModelo
-from flask import render_template, flash, redirect, url_for
+from ...utils.flash_errors import flash_errors
+from ...cursor import db
 
-class ProcessoEditarNegocio:
-
-    def exibir(processo_id):
+class ProcessoEditarNegocio:    
+    def exibir(processo_id):        
         form = EditarProcessoForm()
 
-        processo = Processo(processo_id)
-        if processo.get_id() is None:
-            return redirect(url_for('processo_listar'))
+        processo = Processo(processo_id = processo_id)
 
-        documentos = ZeldaModelo.lista_documentos()
-        form.documentos_ids.choices = [(d.get_id(), d.nome) for d in documentos]
+        if request.method == 'GET':
 
-        if form.validate_on_submit():
-            processo.tipo = form.processo_tipo.data
-            processo.desc = form.processo_desc.data
+            if processo.get_id() is not None:
+                form.processo_descricao.data = processo.descricao
+                form.processo_id.data = processo.get_id()
+            else:
+                return redirect(url_for('processo_listar_13'))
+
+        elif form.validate_on_submit():
+            processo.descricao = form.processo_descricao.data
             processo.salva()
 
-            processo.altera_documentos(form.documentos_ids.data)
-            return redirect(url_for('processo_listar'))
+            return redirect(url_for('processo_listar_13'))
         else:
             flash_errors(form)
 
-        form.processo_tipo.data = processo.tipo
-        form.processo_desc.data = processo.desc
-        return render_template('processo_editar.html', form=form)
+        return render_template('processo_editar_13.html', form=form)

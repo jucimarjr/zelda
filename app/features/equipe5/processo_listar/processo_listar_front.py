2c8dc9507c78369134f828683dbdb399c45a6a47
Jailson Pereira <jailsoncolares@gmail.com>
2017-11-18 11:32:27 -0400

    ...

--- app/features/equipe5/processo_listar/processo_listar_front.py
+++ app/features/equipe5/processo_listar/processo_listar_front.py
@@ -3,7 +3,7 @@ from app import app
 from ...utils.front_helper import *
 
 @app.route('/processocinco')
-@login_required
-@verifica_permissao
+#@login_required
+#@verifica_permissao
 def processo_listar_5():
     return ProcessoListarNegocio.exibir()

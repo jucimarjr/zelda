d92447db6c47801600d3fa592b2d745b6927cf86
Jailson Pereira <jailsoncolares@gmail.com>
2017-11-21 09:55:31 -0400

    feat: farcade

---
+++ design_pattern/10_Facade/farcade.py
@@ -0,0 +1,19 @@
+class Cachorro:
+    def falar(self):
+        print ("AU! AU!")
+
+class Gato:
+    def falar(self):
+        print ("MIAU!")
+
+class Chamar:
+    def __init__(self):
+        self.cachorro = Cachorro()
+        self.gato = Gato()
+    def chamar(self):
+        self.cachorro.falar()
+        self.gato.falar()
+
+if __name__ == '__main__':
+    farcade = Chamar()
+    farcade.chamar()

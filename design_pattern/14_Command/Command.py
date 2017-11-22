ffe8e26ca122e8d9788126f74ccb45acec85adcc
Mateus Souza <mota.mateus13@gmail.com>
2017-11-22 14:54:46 -0400

    fixing: atualizando Command

--- design_pattern/14_Command/Command.py
+++ design_pattern/14_Command/Command.py
@@ -1,23 +1,28 @@
 import abc
 
-class Command(metaclass=abc.ABCMeta):
+class Operar(metaclass=abc.ABCMeta):
 	@abc.abstractmethod
 	def operar(self, A, B):
 		pass
 
-class Somar(Command):
+class Somar(Operar):
 	def operar(self, A, B):
 		return (A+B)
 
-class Subtrair(Command):
+class Subtrair(Operar):
 	def operar(self, A, B):
 		return (A-B)
 
-a = 10
-b = 5
+class Command():
 
-somar = Somar()
-subtrair = Subtrair()
+	def calcular(self, a, b):
+		somar = Somar()		
+		subtrair = Subtrair()
+		print('Soma: ', somar.operar(a, b))
+		print('Subtração: ', subtrair.operar(a, b))
 
-print('Soma: ', somar.operar(a, b))
-print('Subtração: ', subtrair.operar(a, b))
+if __name__ == "__main__":
+	a = int(input())
+	b = int(input())
+	command = Command()
+	command.calcular(a, b)

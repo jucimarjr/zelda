e8c4d7dac6cfb9a8442eb7cfd8088666b0e0e382
jessiccatavares <jldst.snf@uea.edu.br>
2017-11-22 16:42:59 -0400

    design_pattern: 09_Decorator

--- design_pattern/09_Decorator/Decorator.py
+++ design_pattern/09_Decorator/Decorator.py
@@ -1,12 +1,15 @@
-#import abc
+import abc
 #class Decorator(metaclass=abc.ABCMeta):
 #@abc.abstractmethod
 
-class Animal:
+class Animal(metaclass=abc.ABCMeta):
 	def __init__(self, nome, idade):
 		super(Animal, self).__init__()
 		self.__nome = nome
 		self.__idade = idade
+	@abc.abstractmethod
+	def falar(self):
+		pass
 
 	def getNome(self):
 		return self.__nome
@@ -22,12 +25,7 @@ class Animal:
 
 	nome = property(fget = getNome, fset = setNome)
 	idade = property(fget = getIdade, fset = setIdade)
-
-
-	'''@abstractmethod
-    def falar(self):
-    	pass'''
-
+	
 class Gato(Animal):
 	def __init__(self, nome, idade):
 		super(Gato, self).__init__(nome, idade)
@@ -45,9 +43,7 @@ class Cachorro(Animal):
 
 class Pet(Animal):
 	def __init__(self, animal):
-		self.animal = Animal()
-		#super(Pet, self).__init__(animal.getNome(), animal.getIdade())
-		super(Pet, self).__init__()
+		super(Pet, self).__init__(animal.getNome(), animal.getIdade())
 		self.animal = animal
 	
 	def falar(self):
@@ -65,27 +61,12 @@ class Pet(Animal):
 	def setNome(self, nome):
 		self.animal.setNome(nome)
 
-
-class Decorator:
-	def __init__(self):
-		self.cachorro = Cachorro("Rex", 9)
-		self.gato = Gato ("Lili", 6)
-		self.pet1 = Pet(cachorro)
-		self.pet2 = Pet(gato)
-		
-		'''
-			Animal dog = new Dog("Rex", 7);
-			Animal cat = new Cat("Lili", 5);
-			Animal pet1 = new Pet(dog);
-			Animal pet2 = new Pet(cat);
-			pet1.falar();
-			pet2.falar();
-		'''
-	
-	def run(self):
-		pet1.falar();
-		pet2.falar();
-
 if __name__ == '__main__':
-	decorator = Decorator()
-	decorator.run()
+	dog = Cachorro("Rex", 7)
+	cat = Gato("Lili", 5)
+
+	pet1 = Pet(dog)
+	pet2 = Pet(cat)
+
+	pet1.falar()
+	pet2.falar()

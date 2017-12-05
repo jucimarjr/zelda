acd963ee010413af1523a29cabf508a3f3bd502a
dudemaiaf <dudu_maia13@hotmail.com>
2017-12-05 14:26:07 -0400

    Update: código e UML gumball Machine DP State

--- design_pattern/20_State/Patter_State_Exemplo.py
+++ design_pattern/20_State/Patter_State_Exemplo.py
@@ -1,20 +1,24 @@
-import gumballMachine
+from gumballMachine import *
 
-global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
 gumball_machine = gumballMachine(5)
+print (gumball_machine.__str__())
 
 gumball_machine.insertQuarter()
 gumball_machine.turnCrank()
+print (gumball_machine.__str__())
 
 gumball_machine.insertQuarter()
 gumball_machine.ejectQuarter()
 gumball_machine.turnCrank()
+print (gumball_machine.__str__())
 
 gumball_machine.insertQuarter()
 gumball_machine.turnCrank()
 gumball_machine.insertQuarter()
 gumball_machine.turnCrank()
 gumball_machine.ejectQuarter()
+print (gumball_machine.__str__())
 
 gumball_machine.insertQuarter()
 gumball_machine.insertQuarter()
@@ -23,3 +27,4 @@ gumball_machine.insertQuarter()
 gumball_machine.turnCrank()
 gumball_machine.insertQuarter()
 gumball_machine.turnCrank()
+print (gumball_machine.__str__())
--- design_pattern/20_State/gumballMachine.py
+++ design_pattern/20_State/gumballMachine.py
@@ -1,21 +1,33 @@
+from random import randint
 class gumballMachine:
-    global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+    global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
     SOLD_OUT = 0  # NAO MUDA DE VALOR
     NO_QUARTER = 1  # NAO MUDA DE VALOR
     HAS_QUARTER = 2  # NAO MUDA DE VALOR
     SOLD = 3  # NAO MUDA DE VALOR
+    WINNER_STATE = 4 #NAO MUDA DE VALOR
     state = SOLD_OUT
     count = 0
-    print("Wayne Enterprises.")
+    result = ""
     def __init__(self,count):
-        global state, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+        global state, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
         self.count = count
         if(count > 0):
             state = NO_QUARTER
-        #return None
+        else:
+            state = SOLD_OUT
+
+    def winnerState (self):
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
+        total = randint(1,10)
+        if(total == 1 and count >= 2):
+            state = WINNER_STATE
+        else:
+            state = state
+
 
     def insertQuarter (self):
-        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
         if(state == HAS_QUARTER):
             print ("Voce nao pode inserir um quarto.")
         elif(state == SOLD_OUT):
@@ -23,11 +35,18 @@ class gumballMachine:
         elif(state == SOLD):
             print ("Aguarde, nos ja estamos lhe dando um chiclete.")
         elif(state == NO_QUARTER):
-            state = HAS_QUARTER
-            print ("Voce inseriu um quarto.")
+            if(self.count > 0):
+                state = HAS_QUARTER
+                print ("Voce inseriu um quarto.")
+            else:
+                print("Voce nao pode inserir um quarto, a maquina esta esgotada.")
+                state = SOLD_OUT
+        elif(state == WINNER_STATE):
+            state = NO_QUARTER
+
 
     def ejectQuarter (self):
-        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
         if(state == HAS_QUARTER):
             print ("Quarto retornado.")
             state = NO_QUARTER
@@ -37,33 +56,61 @@ class gumballMachine:
             print ("Desculpe, você já virou a manivela.")
         elif(state == NO_QUARTER):
             print ("Voce nao inseriu um quarto.")
+        elif(state == WINNER_STATE):
+            state = WINNER_STATE
+
 
     def turnCrank(self):
-        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
         if(state == SOLD):
             print ("Virar duas vezes nao te dara outro chiclete.")
         elif(state == SOLD_OUT):
             print ("Voce virou, mas nao ha chicletes.")
         elif(state == HAS_QUARTER):
-            print ("Voce virou...")
+            print("Voce virou...")
+            gumballMachine.winnerState(self)
+            if(state == WINNER_STATE):
+                print("Voce ganhou um chiclete a mais.")
+                state = SOLD
+                gumballMachine.dispense(self)
             state = SOLD
             gumballMachine.dispense(self)
         elif(state == NO_QUARTER):
             print ("Voce virou mas nao tem um quarto.")
+        elif(state == WINNER_STATE):
+            print("'-'")
+
+
 
     def dispense(self):
-        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
         if(state == SOLD):
-            print("O chiclete esta rolando para fora do slot.")
-            count = count - 1
-            if(count == 0):
+            if(self.count > 0):
+                print("O chiclete esta rolando para fora do slot.")
+                self.count = self.count - 1
+                state = NO_QUARTER
+            else:
                 print("Oops, sem chicletes!")
                 state = SOLD_OUT
-            else:
-                state = NO_QUARTER
+
         elif(state == NO_QUARTER):
             print("Voce tem que pagar primeiro.")
         elif(state == SOLD_OUT):
             print("Nenhum chiclete dispensado.")
         elif(state == HAS_QUARTER):
             print("Nenhum chiclete dispensado.")
+        elif(state == WINNER_STATE):
+            print("Voce já ganhou seu chiclete extra.")
+
+    def __str__(self):
+        global state, count, SOLD_OUT, NO_QUARTER, HAS_QUARTER, SOLD, WINNER_STATE
+        result = ""
+        result += "\nO Incrivel mundo de Gumball, Inc."
+        result += "\nPython-enabled Stading Gumball Model #2017"
+        result += "\nInventory: " + str(self.count) + " gumball"
+
+        if self.count == 1:
+            result += "s"
+        result += "\n"
+        result += "Machine is " + str(state) + "\n"
+        return result
